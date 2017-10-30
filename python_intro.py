9939187de1960dec32f8025aa0d8819e217e159b
vlasov <vlasovps@gmail.com>
2017-10-30 20:38:12 +0300

    My mylag app, first commit

---
+++ python_intro.py
@@ -0,0 +1,9 @@
+def hi(name):
+    print ('Hi ' + name + '!')
+girls = ['Masha', 'Sasha', 'Glasha', 'Dasha', 'You']
+for name in girls:
+    hi(name)
+    print ("Next pizda")
+
+for i in range(1, 6):
+    print (i)
